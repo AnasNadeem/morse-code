8aa0a862d4bccfd2f712bf095c690a01e795012e
Anas Nadeem <57933598+AnasNadeem@users.noreply.github.com>
2021-11-06 12:26:37 +0000

    Modified app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+function MorseCode(){
+  const [text, fullText] = useState('');
+
+  function covertCode(){
+    const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  }
+
+  return (
+    <div>
+      <label>Enter text below:</label>
+      <input type="text" id="textInp" onChange={() => fullText(textInp.value)}></input>
+      <h2>Output</h2>
+      <p id="outputPara">{text}</p>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <main>
+    <div>
       This is Morse code translator app.
-    </main>
+      <MorseCode />
+    </div>
   );
 }
 
