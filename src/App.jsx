63d79677f7f9c9ae29d7f1c9acbc307e95c179c3
Anas Nadeem <57933598+AnasNadeem@users.noreply.github.com>
2021-11-18 08:11:02 +0000

    Modified ConvertCode

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,30 @@ import './App.css';
 function MorseCode(){
   const [text, fullText] = useState('');
 
-  function covertCode(){
+  function covertCode(textInp){
     const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+    const morseDict = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
+
+    morseValue = ""
+    for(let i=0;i<textInp.length;i++){
+      letter = text[i]
+      for(const letter of alphabet){
+        morseValue+=morseDict[i]
+        console.log(morseDict[i])
+      }
+    }
+
+    return morseValue;
   }
+
 
   return (
     <div>
       <label>Enter text below:</label>
       <input type="text" id="textInp" onChange={() => fullText(textInp.value)}></input>
       <h2>Output</h2>
-      <p id="outputPara">{text}</p>
+      <p id="outputPara">{(text) => convertCode(text)}</p>
     </div>
   )
 }
